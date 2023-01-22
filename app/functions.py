32b61cc4b03a715b8c22155e40d5cdaa3a636d1e
Belal237 <oumar.belal@gmail.com>
2023-01-22 20:14:03 +0100

    update main

--- app/functions.py
+++ app/functions.py
@@ -167,10 +167,10 @@ class SupervisedModel:
             tags(list): Liste des tags predits par le model
         """
         text_list = [text]
-        #vectorisation = self.tfidf_vectorizer.transform(text)
-        vectorisation = vectorisation_USE(text_list)
-        #input_vector = pd.DataFrame(vectorisation.toarray(), columns=self.tfidf_vectorizer.get_feature_names_out())
-        input_vector = vectorisation.numpy()
+        vectorisation = self.tfidf_vectorizer.transform(text)
+        #vectorisation = vectorisation_USE(text_list)
+        input_vector = pd.DataFrame(vectorisation.toarray(), columns=self.tfidf_vectorizer.get_feature_names_out())
+        #input_vector = vectorisation.numpy()
         res = self.supervised_model_use.predict(input_vector)
         res = self.mlb_model.inverse_transform(res)
         tag = list({tag for tag_list in res for tag in tag_list if (len(tag_list) != 0)})
